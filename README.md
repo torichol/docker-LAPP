c5ec469e8d6b9d0cc90b3fdd25c29e24486fdf63
torichol <fujimoto@omakase.com>
2018-11-23 10:20:33 +0900

    1st step

---
+++ README.md
@@ -0,0 +1,27 @@
+# docker-compose で LAPP 環境を構築する（未完成）  
+  
+## docker-compose を用いて PHP7.2.12 + PostgreSQL11.1 環境を構築するコンテナ群です（未完成）  
+  
+## 未完成！  
+phpinfo() の表示と PHP ⇒ PostgreSQL の接続はできていますが、PostgreSQL の日本語環境での initdb が上手く行かず、データベースの言語環境は en_US.utf8 のままです...(-_-)。  
+解決次第、更新します。  
+  
+## 作成＆確認環境  
+<dl>
+  <dt>OS</dt>
+  <dd>Windows 10 Pro</dd>
+  <dt>Docker for Windows</dt>
+  <dd>Version 2.0.0.0-win78(28905)</dd>
+  <dt>Docker</dt>
+  <dd>Docker version 18.09.0, build 4d60db4</dd>
+  <dt>docker-compose</dt>
+  <dd>docker-compose version 1.23.1, build b02f1306</dd>
+  <dt>その他</dt>
+  <dd>Kitematicなど</dd>
+</dl>
+  
+## 使い方  
+*作成中*
+  
+*by FUJIMOTO（torichol）＠2018/11/23*  
+  
---
+++ html/index.php
@@ -0,0 +1,37 @@
+<?php
+echo("docker-compose による LAPP 環境作成<br />\n");
+
+//▼DB初期値
+$dbHOST="pgsql";																//▼主DBホスト名
+$dbPORT="5432";																//▼主DBポート番号
+$dbNAME="zip7";																//▼主DB名
+$dbUSER="pguser";																//▼主DBアクセスユーザ名
+$dbPWD="pguser";																//▼主DBＰＷＤ
+
+//◆データベース接続
+$db1Connect=pg_Connect("host=".$dbHOST." port=".$dbPORT." dbname=".$dbNAME." user=".$dbUSER." password=".$dbPWD);
+//▼ＤＢ接続チェック
+if(!$db1Connect){
+	echo("PostgreSQLデータベース接続に失敗しました...(-_-)<br />\n");
+}else{
+	echo("PostgreSQLデータベース接続成功＼(^o^)／<br />\n");
+}
+
+echo("　<br />\n");
+echo("　<br />\n");
+echo("　<br />\n");
+
+phpinfo();
+
+echo("　<br />\n");
+echo("　<br />\n");
+echo("　<br />\n");
+
+$HOGE=gd_info();
+foreach($HOGE as $key => $value){
+	echo($key." => ".$value."<br />\n");
+}
+echo("　<br />\n");
+echo("　<br />\n");
+echo("「JIS-mapped Japanese Font Support」が「OK」になっている場合は、GD の SJIS 変換が有効になっているので、imageTtfText() 関数の修正の必要がありますよー。<br />\n");
+echo("　<br />\n");
